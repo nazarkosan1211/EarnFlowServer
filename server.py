7c59b1ce958c64c89cae4cba641d1511dea47c15
nazarkosan1211 <nazarkosan1@gmail.com>
2026-05-07 20:29:42 +0700

    Update server.py

--- server.py
+++ server.py
@@ -201,4 +201,5 @@ init_db()
 # RUN SERVER
 # ========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
